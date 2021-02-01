108094e7a7f1d689a8416081dea7565b71a8be54
DrErickson <77122859+DrErickson@users.noreply.github.com>
2021-02-01 08:25:15 -0800

    Add files via upload

---
+++ UnsortedList.cpp
@@ -0,0 +1,72 @@
+
+#include "UnsortedList.h"
+
+
+template<class T>
+UnsortedList<T>::UnsortedList() {
+    length = 0;
+    currentPos = -1;  // 0
+}
+
+template<class T>
+void UnsortedList<T>::MakeEmpty() {
+    length = 0;
+}
+
+template<class T>
+bool UnsortedList<T>::IsFull() const {
+    return length == MAX_ITEMS;
+}
+
+template<class T>
+int UnsortedList<T>::GetLength() const {
+    return length;
+}
+
+template<class T>
+bool UnsortedList<T>::Contains(T someItem) {
+
+    for (int i = 0; i < length; i++) {
+        if (info[i] == someItem) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+template<class T>
+void UnsortedList<T>::PutItem(T item) {
+    if (!IsFull()) {
+        info[length++] = item;
+    }
+}
+
+template<class T>
+void UnsortedList<T>::DeleteItem(T item) {
+
+    for (int i = 0; i < length; i++) {
+        if (info[i] == item) {
+            info[i] = info[length - 1];
+            length--;
+            return;
+        }
+    }
+
+}
+
+template<class T>
+void UnsortedList<T>::ResetIterator() {
+    currentPos = -1;
+    //currentPos = 0;
+}
+
+template<class T>
+int UnsortedList<T>::GetNextItem() {
+    if (currentPos >= length) {
+        throw "Out of bounds";
+    }
+    currentPos++;
+    return info[currentPos];
+}
+
